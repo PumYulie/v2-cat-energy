18da179a8bf1bc8d4827aa6ee6bc994354b20c94
PumYulie <yulisakova@gmail.com>
2022-07-14 00:24:31 +0300

    пиксель перфект каталог

---
+++ source/js/app.js
@@ -0,0 +1,8 @@
+/* эта кнопка не нужна)) она просто ссылка)) */
+const firstScreenBtn = document.querySelector(".first-screen__button");
+
+const onOrderButtonClick = function (event) {
+	event.target
+/* когда кликнули в корзине отправить заявку */
+}
+
