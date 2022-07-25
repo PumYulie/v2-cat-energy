e770028bd2dddec6e6a5a7dadabbbbf205e0a104
PumYulie <yulisakova@gmail.com>
2022-07-25 20:29:53 +0300

    начало pixel perfect у индекс

--- source/js/app.js
+++ source/js/app.js
@@ -1,25 +1,46 @@
-/* DOM-элементы */
+const headerNavigation = document.querySelector(".page-header__navigation");
+const formSubmitButton = document.querySelector(".form__button");
+const menuToggle = document.querySelector(".page-header__menu-toggle"); /* кнопка меню */
+const menuToggleSvg = document.querySelector(".menu-toggle__svg");
+const mobileMenuList = document.querySelector(".page-header__menu-list");
 
-/* в form.html финальная кнопка */
-const firstScreenBtn = document.querySelector(".form__button");
+if (headerNavigation.classList.contains("page-header__navigation--nojs")) {
+	headerNavigation.classList.remove("page-header__navigation--nojs");
+}
 
-const menuToggle = document.querySelector(".menu-toggle");
-const siteHeaderNavigation = document.querySelector(".page-header__navigation");
+/* СДЕЛАТЬ НАДПИСЬ */
+if (formSubmitButton) {
+	if (formSubmitButton.hasAttribute("disabled")) {
+		formSubmitButton.removeAttribute("disabled");
+	/* 	убрать полоску, которая говорит что-нибудь про извините, просим перезагрузить сайт */
+	}
+	formSubmitButton.addEventListener("click", evt => onSubmitOrderClick(evt));
+}
 
+const onMenuToggleClick = function (evt) {
+	console.log(evt);
+	/* если меню открыто = на кнопке висит сигнальный класс --menu-openned */
+	if (menuToggle.classList.contains("menu-toggle--menu-openned")) {
+		menuToggle.classList.remove("menu-toggle--menu-openned");
+		/* меняю в use  href чтобы отображалась иконка бургера из спрайта */
+		menuToggleSvg.firstElementChild.setAttribute("href", "./img/vector-sprite.svg#menu-burger-icon");
+		/* вернуть размеры бургера, удалив изменяющий размеры класс */
+		menuToggleSvg.classList.remove("menu-toggle__svg--close-icon");
+		menuToggle.lastElementChild.textContent = "Открыть меню сайта";
+		mobileMenuList.classList.remove("page-header__menu-list--opened");
+	} else {
+		menuToggle.classList.add("menu-toggle--menu-openned");
+		/* 	меняю классом размеры svg под закрывающий крестик */
+		menuToggleSvg.classList.add("menu-toggle__svg--close-icon");
+		/* меняю в use href на #menu-close-icon, чтобы отображался крестик из спрайта */
+		menuToggleSvg.firstElementChild.setAttribute("href", "./img/vector-sprite.svg#menu-close-icon");
+		menuToggle.lastElementChild.textContent = "Закрыть меню сайта";
+		mobileMenuList.classList.add("page-header__menu-list--opened");
+	}
+}
 
-/* функции-обработчики */
-
-/* кликнули в корзине отправить заявку */
 const onSubmitOrderClick = function (evt) {
-
+	alert(evt.target);
 }
 
-const onMenuToggleClick = function () {
-	siteHeaderNavigation.classList.remove("page-header__navigation--nojs");
-
-	if ()
-}
-
-/* навешиваю счетчики */
-firstScreenBtn.addEventListener("click", onSubmitOrderClick);
-menuToggle.addEventListener("click", onMenuToggleClick);
+menuToggle.addEventListener("click", evt => onMenuToggleClick(evt));
